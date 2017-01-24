a356f3cd44a7bc179bafdfc0c9bf09ef20fea55a
mensinda <daniel@mensinger-ka.de>
2017-01-24 19:03:09 +0100

    Fixed compile error

--- dataCollect/CycleBuilder.cpp
+++ dataCollect/CycleBuilder.cpp
@@ -36,7 +36,7 @@ namespace EPL_DataCollect {
 // Constructors/Destructors
 //
 
-CycleBuilder::CycleBuilder() { return thisIsAnError; }
+CycleBuilder::CycleBuilder() {}
 
 CycleBuilder::~CycleBuilder() {}
 
