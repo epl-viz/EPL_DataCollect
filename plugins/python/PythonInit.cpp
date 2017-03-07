a10dbbad51175f12c2a03ef192a9efd591a73576
Daniel Mensinger <daniel@mensinger-ka.de>
2017-03-07 15:22:19 +0100

    Added PythonInit class

---
+++ plugins/python/PythonInit.cpp
@@ -0,0 +1,58 @@
+/* Copyright (c) 2017, EPL-Vizards
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the EPL-Vizards nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL EPL-Vizards BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/*!
+ * \file PythonInit.cpp
+ * \brief Contains class PythonInit
+ */
+
+#include "PythonInit.hpp"
+
+#include "Python.h"
+
+namespace EPL_DataCollect {
+namespace plugins {
+
+using namespace constants;
+
+PythonInit::PythonInit() {
+  Py_Initialize();
+  PyRun_SimpleString("import sys\n");
+  addPath(EPL_DC_CM_BINARY_DIR + "/lib");
+  addPath(EPL_DC_BUILD_DIR_ROOT + "/python/plugins");
+  addPath(EPL_DC_BUILD_DIR_ROOT + "/python/plugins/tests");
+  addPath(EPL_DC_BUILD_DIR_ROOT + "/python/plugins/tests/loadtests");
+}
+
+PythonInit::~PythonInit() { Py_Finalize(); }
+
+void PythonInit::addPath(std::string path) {
+  std::string temp = "sys.path.append('";
+  temp += path;
+  temp += ")\n";
+  PyRun_SimpleString(temp.c_str());
+}
+}
+}
