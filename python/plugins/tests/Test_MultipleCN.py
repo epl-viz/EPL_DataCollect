aaf05cd32e3211a0390632761b8d161c322fcf28
Denis Megerle <megerle.denis@googlemail.com>
2017-03-07 16:07:32 +0100

    added last python test for coming multiple cn test file

---
+++ python/plugins/tests/Test_MultipleCN.py
@@ -0,0 +1,15 @@
+import Plugin                             # make sure Plugin cython lib is imported
+
+class Test_MultipleCN(Plugin.Plugin):       # name your class plugin like your python module, and let it derive from Plugin.Plugin !
+  def getID(self):
+    return "Test_MultipleCN"                # make sure the getID method returns the exact class name as a str
+
+  def initialize(self):                   # (optional) a initialize method, returning true
+    return True                             # register your cyclestorages and needed data here
+
+  def run(self):                          # (optional, but without it well...) run method
+    pass   ## your code here
+
+  def getDependencies(self):              # (optional) this method takes other plugin names as str and makes sure those are there, too
+    return ""
+
