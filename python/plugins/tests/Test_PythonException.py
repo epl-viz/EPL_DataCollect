c68f98c2e57ad9f77da2f229ff60bc369d90c779
Denis Megerle <megerle.denis@googlemail.com>
2017-03-07 17:27:12 +0100

    changed testcase pythonexception

--- python/plugins/tests/Test_PythonException.py
+++ python/plugins/tests/Test_PythonException.py
@@ -1,6 +1,7 @@
 import Plugin                             # make sure Plugin cython lib is imported
+import unittest
 
-class Test_PythonException(Plugin.Plugin):       # name your class plugin like your python module, and let it derive from Plugin.Plugin !
+class Test_PythonException(Plugin.Plugin, unittest.TestCast):       # name your class plugin like your python module, and let it derive from Plugin.Plugin !
   def getID(self):
     return "Test_PythonException"                # make sure the getID method returns the exact class name as a str
 
@@ -9,6 +10,8 @@ class Test_PythonException(Plugin.Plugin):       # name your class plugin like y
 
   def run(self):                          # (optional, but without it well...) run method
     i = int(23, 23, 23, 23, 23)
+    if self.getCycle().getCycleNum() > 1:
+      self.fail("[python] exceptions should stop the specific plugin !")
 
   def getDependencies(self):              # (optional) this method takes other plugin names as str and makes sure those are there, too
     return ""
