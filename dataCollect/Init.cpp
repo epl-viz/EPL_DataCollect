7820bc0f7050134c5cf83a162b1c2bf486a2d6d7
mensinda <daniel@mensinger-ka.de>
2017-03-26 21:45:46 +0200

    Removed LD_LIBRARY_PATH hack

--- dataCollect/Init.cpp
+++ dataCollect/Init.cpp
@@ -40,7 +40,7 @@ namespace EPL_DataCollect {
 
 using namespace constants;
 
-Init::Init(std::string pluginsDir, std::string wiresharkLibDir) {
+Init::Init(std::string pluginsDir) {
   init_process_policies();
 
   if (started_with_special_privs() || running_with_special_privs()) {
@@ -54,17 +54,6 @@ Init::Init(std::string pluginsDir, std::string wiresharkLibDir) {
       return;
     }
   }
-
-  char *currentLD = getenv("LD_LIBRARY_PATH");
-  if (currentLD) {
-    std::string temp(currentLD);
-    if (!temp.empty()) {
-      wiresharkLibDir += ":";
-      wiresharkLibDir += temp;
-    }
-  }
-
-  auto ret0 = setenv("LD_LIBRARY_PATH", wiresharkLibDir.c_str(), 1);
 
   if (ws_dissect_plugin_dir(pluginsDir.c_str()) != TRUE) {
     std::cerr << "Failed to set the wireshark plugin dir" << std::endl;
@@ -81,13 +70,11 @@ Init::Init(std::string pluginsDir, std::string wiresharkLibDir) {
   ws_dissect_proto_disable("epl"); // Disable old dissector
 
   std::cout << "[Init] Wireshark plugins dir:             " << pluginsDir << std::endl;
-  std::cout << "[Init] new LD_LIBRARY_PATH:               " << wiresharkLibDir << std::endl;
-  std::cout << "[Init] setenv returned:                   " << ret0 << std::endl;
   std::cout << "[Init] ws_capture_init returned:          " << ret1 << std::endl;
   std::cout << "[Init] ws_dissect_init returned:          " << ret2 << std::endl;
   std::cout << "[Init] Loaded the advanced EPL dissector: " << foundProto << std::endl;
 
-  if (ret0 != 0 || ret1 != 0 || ret2 != 0) {
+  if (ret1 != 0 || ret2 != 0) {
     errorCode = WIRESHARK_INIT_FAILED;
     return;
   }
